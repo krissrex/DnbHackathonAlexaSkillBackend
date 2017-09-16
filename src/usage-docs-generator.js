3c86ae1838b6099cd87f916c313cde7a2f3bc9b8
Kristian Rekstad <krirek@msn.com>
2017-09-16 17:19:25 +0200

    Fix docs generator file creation

--- src/usage-docs-generator.js
+++ src/usage-docs-generator.js
@@ -1,25 +1,44 @@
 const mkdir = require('mkdirp')
 const fs = require('fs')
-const intents = require("alexa-skill.json").intents
+const intents = require("./alexa-skill/intents.json").intents
 
-const docFileFolder = '../dist/docs/'
-const docFilePath = docFileFolder + 'usage.md'
 
-mkdir(docFileFolder, err => {
-  if (err) {
-    console.error("Failed to create dosc path: ", error)
-  } else {
-    console.log(`Created docs path: "${docFileFolder}"`)
-  }
+
+const createFolder = folder => new Promise((resolve, reject) => {
+  mkdir(folder, err => {
+    if (err) {
+      console.error("Failed to create docs path: ", error)
+      return reject(err)
+    } else {
+      console.log(`Created docs path: "${folder}"`)
+      return resolve(folder)
+    }
+  })
 })
 
 
-docs = ``
-
-fs.writeFile(docFilePath, docs, err => {
-  if (err) {
-    console.error("Failed to create docs: ", err)
-  } else {
-    console.log(`Wrote docs to "${docFilePath}"`)
-  }
+write = (data, path) => new Promise((resolve, reject) => {
+  fs.writeFile(path, data, err => {
+    if (err) {
+      console.error("Failed to create docs: ", err)
+      return reject(err)
+    } else {
+      console.log(`Wrote docs to "${path}"`)
+      return resolve(path)
+    }
+  })
 })
+
+
+docs = 
+`# Usage examples
+
+
+`
+const docsDirectory = 'dist/docs/'
+const docsFileName = 'usage.md'
+
+createFolder(docsDirectory)
+  .then(folder => write(docs, folder + docsFileName))
+  .then(() => console.log("Done"))
+  .catch(err => console.error(err))
